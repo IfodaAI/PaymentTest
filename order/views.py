c582d5b35102a67ea7979c8a9b8c57ee159113d3
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-08-05 10:32:15 +0500

    Keys update

--- order/views.py
+++ order/views.py
@@ -31,7 +31,7 @@ class OrderViewSet(ModelViewSet):
             merchant_id="30842",
             merchant_user_id="61355",
             secret_key="KbcSKFP7TDVe",
-            is_test_mode=False
+            is_test_mode=True
         )
         return click.create_payment(
             id=data['id'],
