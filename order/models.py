b4a4dea39afaad2a1d1d8a8ba1f48ad966ee99b5
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-08-07 17:11:48 +0500

    order model update

--- order/models.py
+++ order/models.py
@@ -16,4 +16,4 @@ class Order(models.Model):
     created_at = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
-        return f"{self.id} - {self.product_name} ({self.amount})"
+        return f"{self.id} - {self.product_name} ({self.amount})-{self.status}"
