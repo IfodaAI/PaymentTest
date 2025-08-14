caee34fd88e0b2f6454785119a1465f7d081ac39
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-08-14 10:16:45 +0500

    PaymeWebhookView update

--- payment/views.py
+++ payment/views.py
@@ -3,16 +3,7 @@ from paytechuz.integrations.django.views import BasePaymeWebhookView, BaseClickW
 from order.models import Order
 
 class PaymeWebhookView(BasePaymeWebhookView):
-
     def before_check_perform_transaction(self, params, account):
-        """
-        Called before checking if a transaction can be performed.
-
-        Args:
-            params: Request parameters
-            account: Account object
-        """
-        # This method is meant to be overridden by subclasses
         return {'allow': True,
                 "items":[
                     {
@@ -25,43 +16,6 @@ class PaymeWebhookView(BasePaymeWebhookView):
                         "package_code": "1470979"
                     }
                 ]}
-    # def check_transaction(self, params, transaction):
-    #     """
-    #     Called when checking a transaction.
-
-    #     Args:
-    #         params: Request parameters
-    #         transaction: Transaction object
-    #     """
-    #     # This method is meant to be overridden by subclasses
-    #     return {
-    #         'transaction': transaction.transaction_id,
-    #         'state': transaction.state,
-    #         'create_time': int(transaction.created_at.timestamp() * 1000),
-    #         'perform_time': (
-    #             int(transaction.performed_at.timestamp() * 1000)
-    #             if transaction.performed_at else 0
-    #         ),
-    #         'cancel_time': (
-    #             int(transaction.cancelled_at.timestamp() * 1000)
-    #             if transaction.cancelled_at else 0
-    #         ),
-    #         "detail":{
-    #             "receipt_type": 0,
-    #             "items":[
-    #                 {
-    #                     "discount": 0,
-    #                     "title": "ENTOSORAN 50 gr",
-    #                     "price": 1500 * 100,
-    #                     "count": 1,
-    #                     "code": "03808001001000000",
-    #                     "vat_percent": 12,
-    #                     "package_code": "1470979"
-    #                 }
-    #             ]
-    #         },
-    #         'reason': transaction.reason,
-    #     }
 
     def successfully_payment(self, params, transaction):
         order = Order.objects.get(id=transaction.account_id)
