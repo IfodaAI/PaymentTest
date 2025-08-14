71a181a35311752117b7961c939f94975fbe2bab
Bekzod4606 <bdmbzd99m27z259p@studenti.unime.it>
2025-08-14 10:08:41 +0500

    PaymeWebhookView update

--- payment/views.py
+++ payment/views.py
@@ -3,29 +3,17 @@ from paytechuz.integrations.django.views import BasePaymeWebhookView, BaseClickW
 from order.models import Order
 
 class PaymeWebhookView(BasePaymeWebhookView):
-    def check_transaction(self, params, transaction):
+
+    def before_check_perform_transaction(self, params, account):
         """
-        Called when checking a transaction.
+        Called before checking if a transaction can be performed.
 
         Args:
             params: Request parameters
-            transaction: Transaction object
+            account: Account object
         """
         # This method is meant to be overridden by subclasses
-        return {
-            'transaction': transaction.transaction_id,
-            'state': transaction.state,
-            'create_time': int(transaction.created_at.timestamp() * 1000),
-            'perform_time': (
-                int(transaction.performed_at.timestamp() * 1000)
-                if transaction.performed_at else 0
-            ),
-            'cancel_time': (
-                int(transaction.cancelled_at.timestamp() * 1000)
-                if transaction.cancelled_at else 0
-            ),
-            "detail":{
-                "receipt_type": 0,
+        return {'allow': True,
                 "items":[
                     {
                         "discount": 0,
@@ -36,10 +24,44 @@ class PaymeWebhookView(BasePaymeWebhookView):
                         "vat_percent": 12,
                         "package_code": "1470979"
                     }
-                ]
-            },
-            'reason': transaction.reason,
-        }
+                ]}
+    # def check_transaction(self, params, transaction):
+    #     """
+    #     Called when checking a transaction.
+
+    #     Args:
+    #         params: Request parameters
+    #         transaction: Transaction object
+    #     """
+    #     # This method is meant to be overridden by subclasses
+    #     return {
+    #         'transaction': transaction.transaction_id,
+    #         'state': transaction.state,
+    #         'create_time': int(transaction.created_at.timestamp() * 1000),
+    #         'perform_time': (
+    #             int(transaction.performed_at.timestamp() * 1000)
+    #             if transaction.performed_at else 0
+    #         ),
+    #         'cancel_time': (
+    #             int(transaction.cancelled_at.timestamp() * 1000)
+    #             if transaction.cancelled_at else 0
+    #         ),
+    #         "detail":{
+    #             "receipt_type": 0,
+    #             "items":[
+    #                 {
+    #                     "discount": 0,
+    #                     "title": "ENTOSORAN 50 gr",
+    #                     "price": 1500 * 100,
+    #                     "count": 1,
+    #                     "code": "03808001001000000",
+    #                     "vat_percent": 12,
+    #                     "package_code": "1470979"
+    #                 }
+    #             ]
+    #         },
+    #         'reason': transaction.reason,
+    #     }
 
     def successfully_payment(self, params, transaction):
         order = Order.objects.get(id=transaction.account_id)
